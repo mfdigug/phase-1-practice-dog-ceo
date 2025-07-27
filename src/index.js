db39afce53f96796caf49f52994cfac630865fc3
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-07-27 15:28:19 +1000

    rendered 4 images to the DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', () => {
+    
+
+fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(res => res.json())
+    .then(data => data.message.forEach(pic => renderPic(pic))
+    )
+
+function renderPic(pic) {
+    const img = document.createElement('img');
+    const imgContainer = document.querySelector('#dog-image-container');
+    img.src = pic
+    imgContainer.appendChild(img)
+}
+})
+
