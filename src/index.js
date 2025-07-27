71b95e1fcf74b2810c54161e47eb70f2266e1c56
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-07-27 16:47:45 +1000

    added li items and click eventListener to change li item color

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-
+//add images to DOM
 fetch('https://dog.ceo/api/breeds/image/random/4')
     .then(res => res.json())
     .then(data => data.message.forEach(pic => renderPic(pic))
@@ -12,5 +12,27 @@ function renderPic(pic) {
     img.src = pic
     imgContainer.appendChild(img)
 }
-})
 
+//add breeds to DOM
+fetch("https://dog.ceo/api/breeds/list/all")
+.then(res => res.json())
+.then(dogs => convertObj(dogs))
+    
+ function convertObj(dogs) {
+    Object.keys(dogs.message).forEach(dog => listDog(dog))
+    // Object.entries(dogs.message).forEach(dog => listDog(dog)) ??
+ }
+
+ function listDog(dog){
+    const li = document.createElement('li')
+    const ul = document.querySelector('#dog-breeds')
+    li.textContent = dog
+    ul.appendChild(li);
+    //add click event to dom => change li colour
+    li.addEventListener('click', (e) => e.target.style.color = "#69f715")
+};
+
+//add functionality to dropdown options
+
+
+})
